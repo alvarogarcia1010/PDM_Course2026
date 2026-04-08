0f987c47a251f92d30dab1d495b7e40980564615
Alvaro Garcia <alvarogarcia1010@gmail.com>
2026-04-08 17:44:44 -0600

    Clase 080426

--- app/src/main/java/com/agarcia/pdm_course_2026/MainActivity.kt
+++ app/src/main/java/com/agarcia/pdm_course_2026/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.agarcia.pdm_course_2026
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
@@ -11,23 +12,57 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.agarcia.pdm_course_2026.clase080426.TipCalculator
 import com.agarcia.pdm_course_2026.clase_230326.ExerciseOne
 import com.agarcia.pdm_course_2026.clase_250326.LemonadeApp
 import com.agarcia.pdm_course_2026.ui.theme.PDM_Course_2026Theme
 
+private const val TAG = "MainActivity"
+
 class MainActivity : ComponentActivity() {
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
+    Log.d(TAG, "onCreate Called")
     enableEdgeToEdge()
     setContent {
       PDM_Course_2026Theme {
         Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-          LemonadeApp(
+          TipCalculator(
             modifier = Modifier.padding(innerPadding),
             )
         }
       }
     }
+  }
+
+  override fun onStart() {
+    super.onStart()
+    Log.d(TAG, "onStart Called")
+  }
+
+  override fun onResume() {
+    super.onResume()
+    Log.d(TAG, "onResume Called")
+  }
+
+  override fun onRestart() {
+    super.onRestart()
+    Log.d(TAG, "onRestart Called")
+  }
+
+  override fun onPause() {
+    super.onPause()
+    Log.d(TAG, "onPause Called")
+  }
+
+  override fun onStop() {
+    super.onStop()
+    Log.d(TAG, "onStop Called")
+  }
+
+  override fun onDestroy() {
+    super.onDestroy()
+    Log.d(TAG, "onDestroy Called")
   }
 }
 
