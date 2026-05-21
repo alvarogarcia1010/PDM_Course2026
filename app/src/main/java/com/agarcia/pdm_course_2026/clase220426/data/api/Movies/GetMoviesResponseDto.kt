55a29dc1469f8c8244b60f36e5a7de2723c17da7
Alvaro Garcia <alvarogarcia1010@gmail.com>
2026-05-21 08:48:24 -0600

    Clase 18 y 20 -0526

---
+++ app/src/main/java/com/agarcia/pdm_course_2026/clase220426/data/api/Movies/GetMoviesResponseDto.kt
@@ -0,0 +1,12 @@
+package com.agarcia.pdm_course_2026.clase220426.data.api.Movies
+
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class GetMoviesResponseDto(
+  val page: Int,
+  val results: List<MovieDto>,
+  @SerialName("total_pages") val totalPages: Int,
+  @SerialName("total_results") val totalResults: Int
+)
--- app/src/main/java/com/agarcia/pdm_course_2026/clase220426/data/repositories/MovieRepository/MovieApiRepository.kt
+++ app/src/main/java/com/agarcia/pdm_course_2026/clase220426/data/repositories/MovieRepository/MovieApiRepository.kt
@@ -1,17 +1,28 @@
 package com.agarcia.pdm_course_2026.clase220426.data.repositories.MovieRepository
 
-import com.agarcia.pdm_course_2026.clase220426.dummy.dummyMovies
-import com.agarcia.pdm_course_2026.clase220426.model.Movie
-import kotlinx.coroutines.delay
+import com.agarcia.pdm_course_2026.clase220426.data.api.KtorClient
+import com.agarcia.pdm_course_2026.clase220426.data.api.Movies.GetMoviesResponseDto
+import com.agarcia.pdm_course_2026.clase220426.data.api.Movies.MovieDto
+import com.agarcia.pdm_course_2026.clase220426.data.api.Movies.toModel
+import io.ktor.client.call.body
+import io.ktor.client.request.get
+import io.ktor.client.request.parameter
 
 class MovieApiRepository : MovieRepository {
-  override suspend fun getMovies(): List<Movie> {
-    delay(2000)
-    return dummyMovies
+  override suspend fun getMovies(): List<com.agarcia.pdm_course_2026.clase220426.model.Movie> {
+    val response: GetMoviesResponseDto = KtorClient.client.get("movie/popular") {
+      parameter("language", "es-ES")
+      parameter("page", 1)
+    }.body()
+
+    return response.results.map { movieDto -> movieDto.toModel() }
   }
 
-  override suspend fun getMovieById(id: Int): Movie? {
-    delay(5000)
-    return dummyMovies.find { it.id == id }
+  override suspend fun getMovieById(id: Int): com.agarcia.pdm_course_2026.clase220426.model.Movie? {
+    val response: MovieDto = KtorClient.client.get("movie/$id") {
+      parameter("language", "es-ES")
+    }.body()
+
+    return response.toModel()
   }
 }
